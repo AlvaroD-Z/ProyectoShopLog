4d33c2163971dd4b04bb23e0dc24676a4e91a73d
AlvaroD-Z <143042082+AlvaroD-Z@users.noreply.github.com>
2023-08-23 20:36:15 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoShopLog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoShopLog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
